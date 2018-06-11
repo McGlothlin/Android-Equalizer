7a90e40848b3558217297181e61e2879ffb14de1
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2018-06-11 03:55:40 +0100

    poking the eq engine

--- app/src/main/java/com/seanmcglothlin/equalizer/MainActivity.kt
+++ app/src/main/java/com/seanmcglothlin/equalizer/MainActivity.kt
@@ -5,8 +5,8 @@ import android.widget.*
 import android.support.v7.app.AppCompatActivity
 import android.widget.SeekBar
 import android.media.audiofx.*
-
-
+import android.media.MediaPlayer
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,38 +21,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var bassBoost: SeekBar = findViewById(R.id.bassBoostSeekBar) as SeekBar
+
+        var band0: SeekBar = findViewById(R.id.band0) as SeekBar
+        var band1: SeekBar = findViewById(R.id.band1) as SeekBar
+        var band2: SeekBar = findViewById(R.id.band2) as SeekBar
+        var band3: SeekBar = findViewById(R.id.band3) as SeekBar
+        var band4: SeekBar = findViewById(R.id.band4) as SeekBar
+
         enabled = findViewById(R.id.enabledSwitch) as Switch
         reset = findViewById(R.id.resetButton) as Button
 
-        var bassBoost: SeekBar = findViewById(R.id.bassBoostSeekBar) as SeekBar
-        var band1: SeekBar = findViewById(R.id.band1) as SeekBar
-        var band2: SeekBar = findViewById(R.id.band2) as SeekBar
-        var band3: SeekBar = findViewById(R.id.band2) as SeekBar
-        var band4: SeekBar = findViewById(R.id.band2) as SeekBar
-        var band5: SeekBar = findViewById(R.id.band2) as SeekBar
-        var band6: SeekBar = findViewById(R.id.band2) as SeekBar
+        eq = Equalizer(0, MediaPlayer().audioSessionId)
+        bb = BassBoost(0, MediaPlayer().audioSessionId)
 
-        eq = Equalizer(0, 0)
-        bb = BassBoost(0, 0)
+        eq!!.enabled
 
-        band1!!.setOnSeekBarChangeListener(
+        Log.d("SEAN", "Init complete")
+        Log.d("SEAN", "Number of bands on this device: " + eq!!.numberOfBands)
+        Log.d("SEAN", eq!!.getCenterFreq(1).toString())
+
+
+        band0!!.setOnSeekBarChangeListener(
                 object : SeekBar.OnSeekBarChangeListener {
 
                 internal var progress: Int = 0
 
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    Log.d("SEAN", "Band level range: " + eq!!.bandLevelRange[0] + " to " + eq!!.bandLevelRange[1])
+
+                    var newLevel: Short = progress.toShort()
+
+//                    eq!!.setBandLevel(0, (eq!!.bandLevelRange[0] / progress).toShort())
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
 
         })
+
+        Log.d("SEAN", "END TEH DEBUGZ")
 
     }
 
